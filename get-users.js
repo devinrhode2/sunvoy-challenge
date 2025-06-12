a5c46e03e9413ea86d9fd2a06a31db4ff12774de
devinrhode2 <devinrhode2@gmail.com>
2025-06-12 15:52:18 -0500

    log users list done, starting next bit

---
+++ get-users.js
@@ -0,0 +1,18 @@
+;(async () => {
+  const usersList = await (
+    await fetch('https://challenge.sunvoy.com/api/users', {
+      headers: {
+        // TODO: investigate cookie situation...
+        cookie:
+          'user_preferences=eyJ0aGVtZSI6ImxpZ2h0IiwibGFuZ3VhZ2UiOiJlbiIsInRpbWV6b25lIjoiVVRDIiwibm90aWZpY2F0aW9ucyI6dHJ1ZX0%3D; feature_flags=eyJuZXdEYXNoYm9hcmQiOnRydWUsImJldGFGZWF0dXJlcyI6ZmFsc2UsImFkdmFuY2VkU2V0dGluZ3MiOnRydWUsImV4cGVyaW1lbnRhbFVJIjpmYWxzZX0%3D; tracking_consent=accepted; JSESSIONID=d2f6f4f6-3598-4c3a-ae47-77c00ce4d496; _csrf_token=ecc39304cf9817d340735d84c11764cc81d82fb28e7be78aa50d60d8eda790ba; analytics_id=analytics_8aafea501fd867cac2330669c04ffe8f; session_fingerprint=2312bf9954f60dc95f6545b4bd8148e1ede02b3fec26bd6de50dcb2b6b53321a; device_id=device_89a916333a71238896893c05',
+      },
+      body: null,
+      method: 'POST',
+    })
+  ).json()
+
+  console.log({ usersList })
+})().catch((e) => {
+  console.error('Uncaught promise rejection', e)
+  process.exit(1) // Maybe `throw new Error` is more semantic way to crash???
+})
